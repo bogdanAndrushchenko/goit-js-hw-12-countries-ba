2c8b1cb89730b1d680031eb40be4884517777761
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2020-10-01 23:09:55 +0300

    corect any function&template

--- src/js/update-countries-markup.js
+++ src/js/update-countries-markup.js
@@ -2,26 +2,26 @@ import countriesTpl from '../templates/countries.hbs';
 import oneCountryTpl from '../templates/forOneCounty.hbs'
 import refs from './refs';
 
-function updateCountryMarkup(data) { ///countryMarkup
+function updateCountryMarkup(data) { 
   refs.countryContainer.classList.add('countries');
   refs.countryContainer.innerHTML = oneCountryTpl(data);
 
 }
 
-function countriesListMarkup(data) { ///countriesMarkup
+function countriesListMarkup(data) { 
   const markup = countriesTpl(data);
   refs.countriesList.insertAdjacentHTML('beforeend', markup);
 }
 
 
 
-function cleanCountryMarkup() { //clearCountry
+function cleanCountryMarkup() { 
   refs.countryContainer.classList.remove('countries');
   refs.countryContainer.innerHTML = " ";
 }
 
 
-function cleanMarkupBefore() { ///clearBeforeMarkup
+function cleanMarkupBefore() { 
   refs.countriesList.innerHTML = " ";
 
 }
@@ -30,7 +30,7 @@ function cleanInputValue() {
   refs.searchInput.value = " ";
 }
 
-const message = 'Danger!!!'
+const message = 'Too many matches found. Please enter a more specific query!'
 
 
 
